09cfd56261c153c8493b016f84d289c629f10155
Brian Flannery <bflannery@caci.com>
2016-05-04 07:21:17 -0600

    Done with tutorial http://bosh.io/docs/create-release.html

--- from_scratch/go.sh
+++ from_scratch/go.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 releaseName="jenkins-boshrelease-I5H0" ;
+releaseVersion='0.0.1' ;
 
 die() { echo "$1" ; exit 1 ;
 }
@@ -10,6 +11,9 @@ source "general.sh" ;
 main() {
   preparation ;
   step1 ;
+  step2 ;
+  step3 ;
+  step6 ;
 }
 
 preparation() {
@@ -41,8 +45,36 @@ step1() {
     [[ -e $f ]] || execute touch $f ;
   done ;
   git status ;
+  echo -e "\nNOTE: Consider adding and committing any changes.\n" ;
 }
+step2() {
+  echo "Nothing happens." 1>&2 ;
+}
+step3() {
+  for p in 'ardo_app' 'libyaml_0.1.4' 'ruby_1.9.3' ; do
+    [[ -e packages/$p ]] || execute bosh generate package $p ;
+  done ;
+  local d=packages/ardo_app ;
+  [[ -e $d/Gemfile ]] || step3_mkGemfile $d/Gemfile ;
+  ( cd $d ;
+    bundle package ;
+  )
+}
+step3_mkGemfile() {
+  cat > $1 <<'EOF3'
+# https://gist.github.com/antonsoroko/974924e0692aa2171229dafa5f2561b2
+source "http://rubygems.org"
 
+gem "sinatra"
+EOF3
+}
+step6() {
+  execute bosh create release --force --name $releaseName \
+    --version "$releaseVersion" ;
+  bosh target ;
+  echo -e "\nNOTE: Should already be targeting the BOSH Director you want.\n" ;
+  
+}
 
 
 main "$@" ;
