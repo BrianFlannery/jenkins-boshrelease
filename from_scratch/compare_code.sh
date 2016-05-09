506fe1ce71adce34a5d5fe3f607d2f284d3a6a3a
Brian Flannery <bflannery@caci.com>
2016-05-09 16:53:41 -0600

    Separate differences by difference, not source.

--- from_scratch/compare_code.sh
+++ from_scratch/compare_code.sh
@@ -304,7 +304,7 @@ compare_them() {
 
 }
 Diff() {
-  diff -r $1 $2 | perl -ne 'if ( /^(diff|Only)/ ) { print $_ } elsif ( /^[<>]/ ) { print "    $_" } else { print "  $_" }'
+  diff "$@" | perl -ne 'if ( /^(diff|Only)/ ) { print $_ } elsif ( /^[<>]/ ) { print "    $_" } else { print "  $_" }' ;
 }
 
 
